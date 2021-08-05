972531da07dc78a0b4e3407faac160c5091ef804
michalspano <speko.spano@gmail.com>
2021-08-05 10:54:55 +0200

    Fix conflicting submodules

---
+++ Build an App/Lesson2-bonus/Lesson2 (bonus)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2 (bonus)
+//
+//  Created by Michal Špano on 02/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Build an App/Lesson2-revision/Lesson 2(revision)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2(revision)
+//
+//  Created by Michal Špano on 02/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
